4e1a1a7482e78dd2c73e193b09250f4cef5e427e
Berk Kasapoglu <berkkasapoglu1@gmail.com>
2024-01-16 18:50:47 +0300

    feat: yml file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
-const VALUE = 10;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ background: VALUE }}>
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
